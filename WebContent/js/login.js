ad88881329e5f2d79cbdb310bc89d85c5bb669dc
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-11-19 10:17:19 -0200

    Ajusta identação

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -2,18 +2,18 @@ var baseHost = 'http://localhost:8080/medsystem-rest/';
 var host = 'http://localhost:8080/medsystem-rest/services/usuarios/';
 
 function enter() {
-	var data = $("#formLogin").serializeJSON();
-	$.ajax({
-		url: host + 'login/' + data.nomeUsuario + '/' + data.senha,
-		type: 'GET',
-		contentType: 'application/json',
-		data: data,
-		success: function(data) {
-			sessionStorage.setItem("usuarioLogado", JSON.stringify(data.usuario));
-			window.location.href = baseHost + "index.html";
-		},
-		error: function(e) {
-			alert('Login ou senha inválidos, tente novamente!');
-		}
-	});
+  var data = $("#formLogin").serializeJSON();
+  $.ajax({
+    url: host + 'login/' + data.nomeUsuario + '/' + data.senha,
+    type: 'GET',
+    contentType: 'application/json',
+    data: data,
+    success: function(data) {
+      sessionStorage.setItem("usuarioLogado", JSON.stringify(data.usuario));
+      window.location.href = baseHost + "index.html";
+    },
+    error: function(e) {
+      alert('Login ou senha inválidos, tente novamente!');
+    }
+  });
 }
