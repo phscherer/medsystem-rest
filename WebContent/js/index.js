22685176b8bc62184241a923a2226ee03f171118
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-11-19 19:46:06 -0200

    Atualiza versão do jquery

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -8,5 +8,21 @@ function exibirUsuarioLogado() {
 
 function sair() {
   sessionStorage.removeItem("usuarioLogado");
-  window.location.href = baseHost + "login.html";
+  window.location.href = baseHost + 'login.html';
+}
+
+function goMainPage() {
+  window.location.href = baseHost + 'index.html';
+}
+
+function goConsultas() {
+  window.location.href = baseHost + 'consultas/create.html';
+}
+
+function goListPerPatient() {
+  window.location.href = baseHost + 'consultas/list-per-patient.html';
+}
+
+function goDoutores() {
+  window.location.href = baseHost + 'doutores/create.html';
 }
