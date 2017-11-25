107376e3a2694bbfac25ac07f73086321e95e7e0
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-11-25 20:53:18 -0200

    Implementa update de registros

--- WebContent/js/consultas.js
+++ WebContent/js/consultas.js
@@ -85,6 +85,45 @@ function listTodasAsConsultas() {
   });
 }
 
+function carregarConsultaParaEdicao(titulo) {
+  $.ajax({
+    url: hostConsultas + 'consultas/' + titulo,
+    type: 'GET',
+    success: function(data) {
+      var consulta = data.consulta;
+      $('#editId').val(consulta.id);
+      $('#editTitulo').val(consulta.titulo);
+      $('#editData').val(consulta.dataConsulta);
+      $('#editObservacoes').val(consulta.observacoes);
+      $('#editarModalNomeConsulta').html(consulta.titulo);
+      console.log('deu boa!');
+    },
+    error: function(data) {
+      alert('Erro ao carregar a consulta para edição.');
+    }
+  });
+}
+
+function salvarAtualizacaoConsulta() {
+  var data = $('#editarConsultaForm').serializeJSON();
+  data = "{\"consulta\":" + JSON.stringify(data) + "}";
+  
+  $.ajax({
+    url: hostConsultas + 'consultas/',
+    type: 'PUT',
+    contentType: 'application/json',
+    data: data,
+    success: function(data) {
+      $('#editarModal').modal('toggle');
+      $('#editarConsultaForm')[0].reset();
+      listConsultasPorPaciente();
+    },
+    error: function(data) {
+      alert('Erro ao atualizar a consulta!');
+    }
+  });
+}
+
 function segueLinkConsulta(link) {
   $.ajax({
     url: hostConsultas + link,
@@ -104,8 +143,8 @@ function addConsultasNaGrid(consulta) {
                + '<td>' + consulta.titulo + '</td>'
                + '<td>' + consulta.dataConsulta + '</td>'
                + '<td>' + 'Dr. Foo Bar' + '</td>'
-               + "<td width='5%'><span class='glyphicon glyphicon-pencil' aria-hidden='true' style='cursor: pointer' onClick=''></td>"
-		       + "<td width='5%'><span class='glyphicon glyphicon-remove' aria-hidden='true' style='cursor: pointer' onClick='cancelarConsulta(\""+consulta.titulo+"\")'></td>"
+               + "<td width='5%'><img src='../icons/pencil-icon.png' title='Atualizar consulta' width='20px' height='20px' style='cursor: pointer' data-toggle='modal' data-target='#editarModal' onClick='carregarConsultaParaEdicao(\""+consulta.titulo+"\")'></td>"
+		       + "<td width='5%'><img src='../icons/trash-icon.png' title='Excluir consulta' width='20px' height='20px' style='cursor: pointer' onClick='cancelarConsulta(\""+consulta.titulo+"\")'></td>"
            + '</tr>';
   $('#consultasGrid').append(data);
 }
